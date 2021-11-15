008d01af76808efa575e4705c1e6f28062564ca9
Jon Garcia Tamayo <jgarciatam@educacion.navarra.es>
2021-11-15 22:10:14 +0100

    Añadido Empleado.php y probado el metodo mostrar()

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-    include "views/vista_resultado.php"
+	include "views/vista_resultado.php";
+	include "Empleado.php";
+	
+	$empleado = new Empleado("Lola", "Etxegaray", "14-78451589-75");
+	$empleadoDos = new Empleado("Maria", "Behobide", "18-56895475-56");
+	
+	echo $empleado->mostrar() . "<br>";	
+	echo $empleadoDos->mostrar();
 ?>
